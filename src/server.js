cf37d9beeca22405ef9490d5414dcfc6a9968039
Nguyễn Đức Giang <nggiang141@gmail.com>
2023-03-13 22:46:33 +0700

    init nodw

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import { mapOder } from './ultilities/sorts.js'
+
+const app = express()
+
+const hostname = 'localhost'
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.end('<h1>Hello </h1><hr/>')
+})
+
+app.listen(port, hostname, () => {
+    console.log(`Hello trello web ${hostname}: ${port} /`)
+})
---
+++ src/ultilities/sorts.js
@@ -0,0 +1,5 @@
+const mapOder = (array, order, key) => {
+    array.sort((a, b) => order.indexOf(a[key]) - order.indexOf(b[key]))
+    return array
+}
+export { mapOder }
