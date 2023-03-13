71ee066ba764ee0a2a73ce4d5bc73ab39f3b9857
Nguyễn Đức Giang <nggiang141@gmail.com>
2023-03-13 23:40:19 +0700

    fix

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { mapOder } from './ultilities/sorts.js'
+import { mapOder } from '*/ultilities/sorts.js'
 
 const app = express()
 
@@ -7,7 +7,7 @@ const hostname = 'localhost'
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.end('<h1>Hello </h1><hr/>')
+    res.end('<h1>Hello Giang</h1><hr/>')
 })
 
 app.listen(port, hostname, () => {
